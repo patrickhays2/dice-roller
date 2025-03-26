43f6bcc173ee23965c648351e3ad28a392b2db21
patrickhays2 <77924722+patrickhays2@users.noreply.github.com>
2025-03-26 12:11:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='your-package-name',  # Replace with your desired package name
-	version='0.1',            # Start with a version number like 0.1
+	name='dice-roller',  # Replace with your desired package name
+	version='2025.03.26',            # Start with a version number like 0.1
 	packages=find_packages(),  # Automatically find all packages in your project
 	install_requires=[         # List any dependencies your package needs
 		# e.g., 'requests', 'numpy'
