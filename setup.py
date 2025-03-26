6b477fd48fb8c249e83c1fc332d667a0e3b21ed9
patrickhays2 <77924722+patrickhays2@users.noreply.github.com>
2025-03-26 12:14:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='dice-roller',  # Replace with your desired package name
+	name='diceroller',  # Replace with your desired package name
 	version='2025.03.26',            # Start with a version number like 0.1
 	packages=find_packages(),  # Automatically find all packages in your project
 	install_requires=[         # List any dependencies your package needs
